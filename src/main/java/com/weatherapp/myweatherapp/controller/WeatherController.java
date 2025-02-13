9029df5dde1c34ae5d1b62e119eb0c8f097070ab
plutos-world <mubaarak.ibrahim@outlook.com>
2025-02-13 23:35:50 +0000

    first commit

---
+++ src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
@@ -0,0 +1,26 @@
+package com.weatherapp.myweatherapp.controller;
+
+import com.weatherapp.myweatherapp.model.CityInfo;
+import com.weatherapp.myweatherapp.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class WeatherController {
+
+  @Autowired
+  WeatherService weatherService;
+
+  @GetMapping("/forecast/{city}")
+  public ResponseEntity<CityInfo> forecastByCity(@PathVariable("city") String city) {
+
+    CityInfo ci = weatherService.forecastByCity(city);
+
+    return ResponseEntity.ok(ci);
+  }
+
+  //git commit test 
+}
