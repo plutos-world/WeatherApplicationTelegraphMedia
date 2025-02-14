04bdf788c5e132fba050fab7290c336faf61e416
plutos-world <mubaarak.ibrahim@outlook.com>
2025-02-14 12:03:40 +0000

    Unit tests

--- src/test/java/com/weatherapp/myweatherapp/service/WeatherServiceTest.java
+++ src/test/java/com/weatherapp/myweatherapp/service/WeatherServiceTest.java
@@ -1,9 +1,48 @@
 package com.weatherapp.myweatherapp.service;
 
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class WeatherServiceTest {
+import com.weatherapp.myweatherapp.model.CityInfo;
+import com.weatherapp.myweatherapp.model.CityInfo.CurrentConditions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-  // TODO: 12/05/2023 write unit tests
+@ExtendWith(MockitoExtension.class)
+public class WeatherServiceTest {
 
+    @Mock
+    private WeatherService weatherService;
+
+    private CityInfo mockCityInfo;
+    private CurrentConditions mockConditions;
+
+    @BeforeEach
+    void setUp() {
+        mockCityInfo = new CityInfo();
+        mockConditions = new CurrentConditions();
+        
+        // Using reflection to set private fields
+        try {
+            java.lang.reflect.Field sunriseField = CurrentConditions.class.getDeclaredField("sunrise");
+            java.lang.reflect.Field sunsetField = CurrentConditions.class.getDeclaredField("sunset");
+            sunriseField.setAccessible(true);
+            sunsetField.setAccessible(true);
+            sunriseField.set(mockConditions, "06:00");
+            sunsetField.set(mockConditions, "18:00");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testForecastByCity() {
+        when(weatherService.forecastByCity("London")).thenReturn(mockCityInfo);
+        CityInfo response = weatherService.forecastByCity("London");
+        assertNotNull(response);
+        assertEquals(mockCityInfo, response);
+    }
 }
